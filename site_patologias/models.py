a0609f1a3bf9eb06af821c640c7257f1428d8ac1
Gabriel Vieira Cardoso <gvc20002016@gmail.com>
2022-11-02 09:19:08 -0300

    Criando modelo de Login

--- site_patologias/models.py
+++ site_patologias/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Admin(models.Model):
+    nome = models.CharField(max_length=40)
+    email = models.CharField(max_length=30)
+    senha = models.CharField(max_length=256)
+    criado_em = models.DateTimeField(auto_now_add=True)
+    atualizado_em = models.DateTimeField(auto_now=True)
+
